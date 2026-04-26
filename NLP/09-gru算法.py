c70976ecb8dfedb77098d1a1a73d0b00ca4bb385
evan0088 <477754154@qq.com>
2026-04-26 15:27:16 +0800

    gru lstm update

--- NLP/09-gru算法.py
+++ NLP/09-gru算法.py
@@ -23,7 +23,7 @@ def demo01():
     # batch_first:
     # 默认False->输入和输出的形状为(句子长度, 句子数, 词维度)
     # True->输入和输出的形状为(句子数, 句子长度, 词维度)  后续transformer模型都是这种形状
-    rnn = nn.RNN(input_size=5,
+    rnn = nn.GRU(input_size=5,
                  hidden_size=7,
                  num_layers=1,
                  batch_first=False)
@@ -55,7 +55,7 @@ def demo02():
     # batch_first:
     # 默认False->输入和输出的形状为(句子长度, 句子数, 词维度)
     # True->输入和输出的形状为(句子数, 句子长度, 词维度)  后续transformer模型都是这种形状
-    rnn = nn.RNN(input_size=5,
+    rnn = nn.GRU(input_size=5,
                  hidden_size=7,
                  num_layers=1,
                  batch_first=False)
@@ -90,7 +90,7 @@ def demo03():
     # batch_first:
     # 默认False->输入和输出的形状为(句子长度, 句子数, 词维度)
     # True->输入和输出的形状为(句子数, 句子长度, 词维度)  后续transformer模型都是这种形状
-    rnn = nn.RNN(input_size=5,
+    rnn = nn.GRU(input_size=5,
                  hidden_size=7,
                  num_layers=2,
                  batch_first=False)
@@ -108,7 +108,44 @@ def demo03():
     # hn: 最后一个时间步所有层的隐藏状态值拼接结果
 
 
+# 双向
+def demo04():
+    # 初始化x->(句子长度/词数, 句子数, 词维度)
+    # 3个句子, 每个句子3个词, 词向量维度为5
+    x = torch.randn(size=(3, 3, 5))
+
+    # 实例化RNN层对象
+    # input_size: 输入的词维度 或者 输入层的特征个数
+    # hidden_size: rnn层的隐层维度 或者 隐藏层的特征个数
+    # num_layers: 默认1层, rnn层数 或者 隐藏层层数
+    # batch_first:
+    # 默认False->输入和输出的形状为(句子长度, 句子数, 词维度)
+    # True->输入和输出的形状为(句子数, 句子长度, 词维度)  后续transformer模型都是这种形状
+    rnn = nn.GRU(input_size=5,
+                 hidden_size=7,
+                 num_layers=1,
+                 batch_first=False,
+                 bidirectional=True)
+
+    # 初始化h0(显性初始化), 如果不进行初始化, 默认初始化为0(隐性初始化)
+    # 形状->(隐藏层层数, 句子数, 隐层维度)
+    h0 = torch.zeros(size=(1*2, 3, 7))
+
+    # 调用rnn层对象,生成output和hn
+    ouput, hn = rnn(x, h0)
+    # ouput, hn = rnn(x)
+    print('ouput--->', ouput.shape, ouput)
+    print('hn--->', hn.shape, hn)
+    # output!=hn -> 因为句子中有3个词, 循环3次
+    # output是h1 h2 h3 三个拼接结果concat, hn是最后一个时间步的隐藏状态值
+
+    # -1: 0维度表示词数/句子长度, -1就是取每个句子的最后一个词
+    last_h = ouput[-1, :, :]
+    print('last_h--->', last_h.shape, last_h)
+
+
 if __name__ == '__main__':
-    # demo01()
-    # demo02()
+    demo01()
+    demo02()
     demo03()
+    demo04()
---
+++ NLP/10-lstm算法.py
@@ -0,0 +1,56 @@
+# 定义LSTM的参数含义: (input_size/输入特征个数/词向量维度, hidden_size/隐层特征个数/隐层维度, num_layers/隐层层数)
+# 定义输入张量x的参数含义: 默认->(sequence_length/句子长度, batch_size/句子数, input_size/词维度)
+# 定义隐藏层初始张量h0和细胞初始状态张量c0的参数含义: (num_layers * num_directions/隐层层数, batch_size/句子数, hidden_size/隐层维度数)
+# 隐层层数:如果单向就等于LSTM中的num_layers参数值; 如果双向就等于LSTM中的num_layers参数值*2
+# 是否双向: bidirectional参数决定, 默认单向; True:双向
+import torch.nn as nn
+import torch
+
+
+# 单向LSTM
+def dm01_lstm():
+    # 创建LSTM层
+    # 将rnn类换成lstm类
+    lstm = nn.LSTM(input_size=5, hidden_size=6, num_layers=1, batch_first=False)
+    # 创建输入张量x 形状->(句子长度, 句子数, 词维度)
+    input = torch.randn(size=(3, 3, 5))
+
+    # c和h形状一致->(隐层数*方向, 句子数, 隐层维度)
+    # 初始化隐藏状态
+    h0 = torch.zeros(size=(1, 3, 6))
+    # 初始化细胞状态
+    c0 = torch.zeros(size=(1, 3, 6))
+
+    # h和c是以数组形式传递和返回
+    # hn输出两层隐藏状态, 最后1个隐藏状态值等于output输出值
+    output, (hn, cn) = lstm(input, (h0, c0))
+    print('output--->', output.shape, output)
+    print('hn--->', hn.shape, hn)
+    print('cn--->', cn.shape, cn)
+
+
+# 单向LSTM
+def dm02_lstm():
+    # 创建LSTM层
+    # 将rnn类换成lstm类
+    lstm = nn.LSTM(input_size=5, hidden_size=6, num_layers=1, batch_first=False, bidirectional=True)
+    # 创建输入张量x 形状->(句子长度, 句子数, 词维度)
+    input = torch.randn(size=(3, 3, 5))
+
+    # c和h形状一致->(隐层数*方向, 句子数, 隐层维度)
+    # 初始化隐藏状态
+    h0 = torch.zeros(size=(1 * 2, 3, 6))
+    # 初始化细胞状态
+    c0 = torch.zeros(size=(1 * 2, 3, 6))
+
+    # h和c是以数组形式传递和返回
+    # hn输出两层隐藏状态, 最后1个隐藏状态值等于output输出值
+    output, (hn, cn) = lstm(input, (h0, c0))
+    print('output--->', output.shape, output)
+    print('hn--->', hn.shape, hn)
+    print('cn--->', cn.shape, cn)
+
+
+if __name__ == '__main__':
+    dm01_lstm()
+    dm02_lstm()
