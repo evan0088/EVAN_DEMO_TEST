bf7e4a3bf03cc34cc278753996a3a4738eba1d0e
evan0088 <477754154@qq.com>
2026-05-15 01:29:00 +0800

    1

--- NLP/transformers的API使用/05-中文文本分类.py
+++ NLP/transformers的API使用/05-中文文本分类.py
@@ -13,11 +13,11 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {device}')
 
 # 加载字典和分词工具 实例化分词工具
-my_tokenizer = BertTokenizer.from_pretrained('model/bert-base-chinese')
+my_tokenizer = BertTokenizer.from_pretrained('../model/bert-base-chinese')
 
 # 加载预训练模型 实例化预训练模型
-my_model_pretrained = BertModel.from_pretrained('model/bert-base-chinese').to(device)
-# print('my_model_pretrained--->', my_model_pretrained)
+my_model_pretrained = BertModel.from_pretrained('../model/bert-base-chinese').to(device)
+print('my_model_pretrained--->', my_model_pretrained)
 
 # 查看预训练模型的输出维度
 hidden_size = my_model_pretrained.config.hidden_size
@@ -28,9 +28,9 @@ print('hidden_size--->', hidden_size)  # 768
 def dm_file2dataset():
     # 实例化数据源对象my_dataset_train
     # print('\n加载训练集')
-    # print(load_dataset('csv', data_files='data/train.csv'))
+    print(load_dataset('csv', data_files='../data/train.csv'))
     # split='train': 获取key对应的value  key的值为train
-    my_dataset_train = load_dataset('csv', data_files='data/train.csv', split='train')
+    my_dataset_train = load_dataset('csv', data_files='../data/train.csv', split='train')
     # print('dataset_train--->', my_dataset_train)
     # print(my_dataset_train[0:3])
     # print('=' * 80)
@@ -39,14 +39,14 @@ def dm_file2dataset():
     # print('\n加载测试集')
     # split='train': 获取key对应的value  key的值为train
     # print(load_dataset('csv', data_files='data/test.csv'))
-    my_dataset_test = load_dataset('csv', data_files='data/test.csv', split='train')
+    my_dataset_test = load_dataset('csv', data_files='../data/test.csv', split='train')
     # print('my_dataset_test--->', my_dataset_test)
     # print(my_dataset_test[0:3])
     # print('=' * 80)
 
     # print('\n加载验证集')
     # 实例化数据源对象my_dataset_train
-    my_dataset_validation = load_dataset('csv', data_files='data/validation.csv', split="train")
+    my_dataset_validation = load_dataset('csv', data_files='../data/validation.csv', split="train")
     # print('my_dataset_validation--->', my_dataset_validation)
     # print(my_dataset_validation[0:3])
     # print('=' * 80)
@@ -74,11 +74,12 @@ def collate_fn(data):
     # print('labels--->', labels)
 
     # 编码text2id 对多句话进行编码用batch_encode_plus函数
-    data = my_tokenizer.batch_encode_plus(batch_text_or_text_pairs=sents,
-                                          truncation=True,
-                                          padding='max_length',
-                                          max_length=500,
-                                          return_tensors='pt')
+    # data = my_tokenizer.batch_encode_plus(batch_text_or_text_pairs=sents,
+    #                                       truncation=True,
+    #                                       padding='max_length',
+    #                                       max_length=500,
+    #                                       return_tensors='pt')
+    data = my_tokenizer(text=sents,truncation=True,padding=True,return_tensors='pt')
     # print('data--->', data)
 
     # input_ids:编码之后的数字
@@ -106,16 +107,21 @@ class MyModel(nn.Module):
         # 预训练模型不训练 只进行特征抽取 [8,500] ---> [8,768]
         with torch.no_grad():
             # 句子的语义表示
+            # 在这之前都是做的预处理
             out = my_model_pretrained(input_ids=input_ids,
                                       attention_mask=attention_mask,
                                       token_type_ids=token_type_ids)
+            # print("out--->",out)
+            # print("out.pooler_output.shape--->",out.pooler_output.shape)
+            # print("out.last_hidden_state.shape--->",out.last_hidden_state.shape)
+
 
         # print('out--->', out)
         # 下游任务模型训练 数据经过全连接层 [8,768] --> [8,2]
         # out.last_hidden_state: 最后一层的隐藏状态张量
         # [:, 0] 选择的是序列的第一个token的隐藏状态
         # 通常这个token是特殊的[CLS]，该token被设计用于表示整个序列的语义。
-        # BERT训练时，特别是文本分类任务，使用[CLS]的表示来作为整个句子的表示。
+        # BERT训练时，特别是文本分类任务，使用[CLS]->[第一个token]的表示来作为整个句子的表示。
         # out = self.fc(out.last_hidden_state[:, 0])
 
         # pooler_output: 通过last_hidden_state[:, 0]拿到[CLS]向量表示后又经过linear层(形状不变)
@@ -136,7 +142,6 @@ def dm01_test():
                                shuffle=True,
                                batch_size=8,
                                drop_last=True)
-
     # 冻结预训练模型 embedding层参数
     # for param in my_model_pretrained.embeddings.parameters():
     #     param.requires_grad_(False)
@@ -157,7 +162,6 @@ def dm01_test():
 
     # 调整数据迭代器对象数据返回格式
     for i, (input_ids, attention_mask, token_type_ids, labels) in enumerate(my_dataloader):
-        # 数据送给模型
         y_out = my_model(input_ids, attention_mask, token_type_ids)
         print('y_out---->', y_out.shape, y_out)
         break
@@ -166,7 +170,7 @@ def dm01_test():
 # todo:4-模型训练
 def train():
     # 实例化数据源 通过训练文件
-    my_dataset_train = load_dataset('csv', data_files='data/train.csv', split="train")
+    my_dataset_train = load_dataset('csv', data_files='../data/train.csv', split="train")
 
     # 实例化数据迭代器对象my_dataloader
     my_dataloader = DataLoader(dataset=my_dataset_train,
@@ -287,5 +291,7 @@ def inference():
 
 
 if __name__ == '__main__':
+    # print("1111")
+    dm01_test()
     # train()
-    inference()
+    # inference()
